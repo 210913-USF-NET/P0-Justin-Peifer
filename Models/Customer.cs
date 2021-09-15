234345be1ead067e5eef083e86ebe7e83f3b148f
Peiferinc <peiferjustin96@gmail.com>
2021-09-15 10:33:38 -0500

    Added a classlib named Models

---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Models
+{
+    public class Customer
+    {
+        public Customer(){}
+
+        public Customer(string name) {
+            this.Name = name;
+        }
+
+        public string Name {get; set;} //property, belongs in a type
+
+        //constructor chaining
+        public Customer(string name, int age) :this(name)
+        {
+            this.Age = age
+        }
+
+        public int Age{get; set;}
+    }
+}
